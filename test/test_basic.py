8d39c538efae0003ea6cd9f43a6bc07c87e1ab4e
benjiDupuis <benjamin.dupuis@polytechnique.edu>
2021-10-24 18:24:34 +0200

    fix imports in pytests

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,4 +1,3 @@
 def test_hello_world():
-    m = 2
 
     _ = "hello world"
--- test/test_topoptim.py
+++ test/test_topoptim.py
@@ -1,6 +1,9 @@
-from topoptim.topopt2D import Topopt2D
+from deeptopo.topoptim.topopt2D import Topopt2D
+from deeptopo.topoptim.loadcase import MichellStructure
 
 
 def test_topopt2D():
 
-    assert True
+    loadcase = MichellStructure((100, 50))
+    optim = Topopt2D(loadcase, 0.4)
+    assert optim.shape == (100, 50), "wrong shape in optimizer"
