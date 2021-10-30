5bcf99006a8371107957efa3b3e369b584cdf834
benjiDupuis <benjamin.dupuis@polytechnique.edu>
2021-10-30 14:43:27 +0200

    define some normalized ctivation functions

--- deeptopo/models/activations.py
+++ deeptopo/models/activations.py
@@ -2,14 +2,37 @@ import numpy as np
 import torch
 
 # Some normalized activation functions
+
+
 def normalized_relu(x):
     return np.sqrt(2.)*torch.relu(x)
 
-def norm_sin(a):
-    return np.sqrt(2./(1 - np.exp(-2.*a*a)))
 
-def norm_cos(a):
-    return np.sqrt(2./(1 + np.exp(-2.*a*a)))
+class NormalizedSin(object):
 
-def norm_exp(a):
-    return np.exp(a**2)
+    def __init__(self, a: float):
+        self.norm = np.sqrt(2./(1 - np.exp(-2.*a*a)))
+        self.omega = a
+
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        return self.norm * torch.sin(self.omega * x)
+
+
+class NormalizedCos(object):
+
+    def __init__(self, a: float):
+        self.norm = np.sqrt(2./(1 + np.exp(-2.*a*a)))
+        self.omega = a
+
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        return self.norm * torch.cos(self.omega * x)
+
+
+class NormalizedExp(object):
+
+    def __init__(self, a: float):
+        self.norm = np.exp(a**2)
+        self.omega = a
+
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        return self.norm * torch.exp(self.omega * x)
