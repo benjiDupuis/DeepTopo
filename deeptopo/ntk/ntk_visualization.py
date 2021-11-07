1e5ab0c1325b284fb912a17a2284e9cbd7aaa621
benjiDupuis <benjamin.dupuis@polytechnique.edu>
2021-11-07 18:18:45 +0100

    add visualization of one line of the empirical ntk

--- deeptopo/ntk/ntk_visualization.py
+++ deeptopo/ntk/ntk_visualization.py
@@ -6,25 +6,29 @@ import numpy as np
 import torch.nn as nn
 
 from deeptopo.models.embeddings import Embedding2D
-from deeptopo.ntk.empirical_ntk import empirical_ntk_1D
+from deeptopo.ntk.empirical_ntk import empirical_NTK_one_line
 
 
 def visualize_ntk(net: nn.Module,
                   shape: Tuple[int],
                   output_dir: str,
-                  embedding: Optional[Embedding2D] = None):
+                  embedding: Optional[Embedding2D] = None,
+                  index: Optional[int] = None):
 
     if embedding is None:
         batch_input = Embedding2D.make_grid(shape)
     else:
         batch_input = embedding(shape)
-    ntk = empirical_ntk_1D(batch_input, net)
-    ntk = ntk.reshape(shape[0], shape[1]).cpu().detach().data.numpy()
+
+    index = shape[1]*(1 + shape[0])//2 if index is None else index
+
+    ntk = empirical_NTK_one_line(batch_input, net, index)
+    ntk = ntk[0].reshape(shape[0], shape[1]).cpu().detach().data.numpy()
 
     ntk = ntk - np.min(ntk)
     if np.max(ntk) >= 1.e-6:
         ntk = ntk/np.max(ntk)
-    ntk = 255*ntk.astype(np.uint8)
+    ntk = 255 - (255*ntk).astype(np.uint8)
 
-    output_path = Path(output_dir) / "ntk.png"
+    output_path = Path(output_dir) / "empirical_ntk.png"
     cv2.imwrite(str(output_path), ntk)
--- test/test_ntk.py
+++ test/test_ntk.py
@@ -1,21 +1,35 @@
 import torch
 
-from deeptopo.ntk.empirical_ntk import empirical_ntk_1D
+from deeptopo.ntk.empirical_ntk import empirical_ntk, empirical_NTK_one_line
 from deeptopo.models.networks import FCNN
+
+
+DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 
 def test_empirical_ntk():
 
-    if torch.cuda.is_available():
-        device = torch.device("cuda")
-    else:
-        device = torch.device("cpu")
+    device = torch.device(DEVICE)
 
     batch_input = torch.rand(3, 2, device=device, dtype=torch.float)
     net = FCNN([10], 2)
     net.to(device)
-    ntk = empirical_ntk_1D(batch_input, net)
+    ntk = empirical_ntk(batch_input, net)
 
     assert len(ntk.shape) == 2, "resulting ntk should be of dim 2"
     assert ntk.shape[0] == 3, "dim 0 of the ntk is wrong"
     assert ntk.shape[1] == 3, "dim 1 of the ntk is wrong"
+
+
+def test_empirical_one_line_ntk():
+
+    device = torch.device(DEVICE)
+
+    batch_input = torch.rand(3, 2, device=device, dtype=torch.float)
+    net = FCNN([10], 2)
+    net.to(device)
+    one_line_ntk = empirical_NTK_one_line(batch_input, net, 0)
+
+    assert len(one_line_ntk.shape) == 2, "resulting ntk should be of dim 2"
+    assert one_line_ntk.shape[0] == 1, "dim 0 of the ntk is wrong"
+    assert one_line_ntk.shape[1] == 3, "dim 1 of the ntk is wrong"
