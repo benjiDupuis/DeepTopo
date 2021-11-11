a6e0c09298d66b3c895f7d0a67421e5974cc7c26
benjiDupuis <benjamin.dupuis@polytechnique.edu>
2021-11-11 12:02:06 +0100

    add dual functions and kernels to the activations and embedding classes

--- deeptopo/models/activations.py
+++ deeptopo/models/activations.py
@@ -1,14 +1,44 @@
+"""
+Definition of normalized activation functions \n
+Their respective dual and derivative dual functions are also implemented \n
+Those last functions can be used to compute the theoretical NTK of the network
+"""
 import numpy as np
 import torch
 
-# Some normalized activation functions
+
+class NormalizedActivation():
+    """
+    Generic class for normalized activations
+    """
+
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        raise NotImplementedError
+
+    @torch.no_grad()
+    def dual(self, rho: torch.Tensor) -> torch.Tensor:
+        raise NotImplementedError
+
+    @torch.no_grad()
+    def dual_derivative(self, rho: torch.Tensor) -> torch.Tensor:
+        raise NotImplementedError
 
 
-def normalized_relu(x):
-    return np.sqrt(2.)*torch.relu(x)
+class NormalizedReLu(NormalizedActivation):
+
+    def __call__(self, x):
+        return np.sqrt(2.)*torch.relu(x)
+
+    @torch.no_grad()
+    def dual(self, rho: torch.Tensor) -> torch.Tensor:
+        return rho - rho*(torch.arccos(rho) - torch.sqrt(1 - rho.pow(2)))/torch.pi
+
+    @torch.no_grad()
+    def dual_derivative(self, rho: torch.Tensor) -> torch.Tensor:
+        return 1. - torch.arccos(rho)/torch.pi
 
 
-class NormalizedSin(object):
+class NormalizedSin(NormalizedActivation):
 
     def __init__(self, a: float):
         self.norm = np.sqrt(2./(1 - np.exp(-2.*a*a)))
@@ -17,8 +47,16 @@ class NormalizedSin(object):
     def __call__(self, x: torch.Tensor) -> torch.Tensor:
         return self.norm * torch.sin(self.omega * x)
 
+    @torch.no_grad()
+    def dual(self, rho: torch.Tensor) -> torch.Tensor:
+        return torch.sinh((self.omega**2)*rho)/np.sinh(self.omega**2)
 
-class NormalizedCos(object):
+    @torch.no_grad()
+    def dual_derivative(self, rho: torch.Tensor) -> torch.Tensor:
+        return (self.omega**2)*torch.cosh((self.omega**2)*rho)/np.sinh(self.omega**2)
+
+
+class NormalizedCos(NormalizedActivation):
 
     def __init__(self, a: float):
         self.norm = np.sqrt(2./(1 + np.exp(-2.*a*a)))
@@ -27,8 +65,16 @@ class NormalizedCos(object):
     def __call__(self, x: torch.Tensor) -> torch.Tensor:
         return self.norm * torch.cos(self.omega * x)
 
+    @torch.no_grad()
+    def dual(self, rho: torch.Tensor) -> torch.Tensor:
+        return torch.cosh((self.omega**2)*rho)/np.cosh(self.omega**2)
 
-class NormalizedExp(object):
+    @torch.no_grad()
+    def dual_derivative(self, rho: torch.Tensor) -> torch.Tensor:
+        return (self.omega**2)*torch.sinh((self.omega**2)*rho)/np.cosh(self.omega**2)
+
+
+class NormalizedExp(NormalizedActivation):
 
     def __init__(self, a: float):
         self.norm = np.exp(a**2)
@@ -36,3 +82,11 @@ class NormalizedExp(object):
 
     def __call__(self, x: torch.Tensor) -> torch.Tensor:
         return self.norm * torch.exp(self.omega * x)
+
+    @torch.no_grad()
+    def dual(self, rho: torch.Tensor) -> torch.Tensor:
+        return super().dual(rho)
+
+    @torch.no_grad()
+    def dual_derivative(self, rho: torch.Tensor) -> torch.Tensor:
+        return super().dual_derivative(rho)
